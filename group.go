a7a62997a7e3dfcfe06edd8eba907800b2624020
Jarri Abidi <jarri.ahmed@careem.com>
2022-10-05 08:55:17 +0500

    Add Group & Contact

---
+++ group.go
@@ -0,0 +1,27 @@
+package gochat
+
+import (
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+type Group struct {
+	id           string
+	name         string
+	participants []Contact
+}
+
+var ErrEmptyGroupName = errors.New("group name cannot be empty")
+
+func NewGroup(name string, participants ...Contact) (*Group, error) {
+	if strings.TrimSpace(name) == "" {
+		return nil, ErrEmptyGroupName
+	}
+
+	return &Group{
+		id:           "", // TODO: generate uuid
+		name:         name,
+		participants: participants,
+	}, nil
+}
--- user.go
+++ user.go
@@ -1,8 +1,9 @@
 package gochat
 
 import (
-	"errors"
 	"strings"
+
+	"github.com/pkg/errors"
 )
 
 type User struct {
@@ -14,8 +15,9 @@ type User struct {
 }
 
 var (
-	ErrEmptyUserName = errors.New("username cannot be empty")
-	ErrEmptyFullName = errors.New("fullname cannot be empty")
+	ErrEmptyID       = errors.New("id cannot be empty")
+	ErrEmptyUserName = errors.New("user name cannot be empty")
+	ErrEmptyFullName = errors.New("full name cannot be empty")
 )
 
 func NewUser(userName, fullName string, contacts ...Contact) (*User, error) {
@@ -28,7 +30,7 @@ func NewUser(userName, fullName string, contacts ...Contact) (*User, error) {
 	}
 
 	return &User{
-		id:       "", // generate uuid
+		id:       "", // TODO: generate uuid
 		userName: userName,
 		fullName: fullName,
 		groups:   make([]Group, 0),
@@ -41,3 +43,29 @@ func (u User) UserName() string    { return u.userName }
 func (u User) FullName() string    { return u.fullName }
 func (u User) Groups() []Group     { return append([]Group{}, u.groups...) }
 func (u User) Contacts() []Contact { return append([]Contact{}, u.contacts...) }
+
+type Contact struct {
+	id       string
+	fullName string
+	userName string
+}
+
+func NewContact(id, fullName, userName string) (*Contact, error) {
+	if strings.TrimSpace(id) == "" {
+		return nil, ErrEmptyID
+	}
+
+	if strings.TrimSpace(userName) == "" {
+		return nil, ErrEmptyUserName
+	}
+
+	if strings.TrimSpace(fullName) == "" {
+		return nil, ErrEmptyFullName
+	}
+
+	return &Contact{id: id, fullName: fullName, userName: userName}, nil
+}
+
+func (c Contact) ID() string       { return c.id }
+func (c Contact) UserName() string { return c.userName }
+func (c Contact) FullName() string { return c.fullName }
